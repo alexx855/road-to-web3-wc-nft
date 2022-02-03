4ee21cfcf81f29eb5270bb8e83982366c59bbece
Alex Pedersen <info@alexpedersen.net>
2022-02-03 22:30:19 +0100

    feat(theme): add child theme, add wc filters

---
+++ wordpress/wp-content/themes/storefront-hackathon-wc-nft/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Storefront automatically loads the core CSS even if using a child theme as it is more efficient
+ * than @importing it in the child theme style.css file.
+ *
+ * Uncomment the line below if you'd like to disable the Storefront Core CSS.
+ *
+ * If you don't plan to dequeue the Storefront Core CSS you can remove the subsequent line and as well
+ * as the sf_child_theme_dequeue_style() function declaration.
+ */
+//add_action( 'wp_enqueue_scripts', 'sf_child_theme_dequeue_style', 999 );
+
+/**
+ * Dequeue the Storefront Parent theme core CSS
+ */
+function sf_child_theme_dequeue_style() {
+    wp_dequeue_style( 'storefront-style' );
+    wp_dequeue_style( 'storefront-woocommerce-style' );
+}
+
+/**
+ * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
+ */
+
+
+add_action('init', 'replace_storefront_primary_navigation' );
+function replace_storefront_primary_navigation(){
+    remove_action( 'storefront_header', 'storefront_primary_navigation', 50 );
+    remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
+    remove_action( 'storefront_before_content', 'woocommerce_breadcrumb', 10 );
+
+    remove_action( 'storefront_header', 'storefront_header_cart', 60 );
+    remove_action( 'storefront_header', 'storefront_product_search', 40 );
+}
+
